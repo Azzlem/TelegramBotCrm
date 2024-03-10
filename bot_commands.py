29bf561597122d7fb9a8085e92742802a68863e3
Azzlem <azzlem.sid@gmail.com>
2024-03-10 14:59:25 +0300

    fix bot command 2

--- bot_commands.py
+++ bot_commands.py
@@ -1,27 +1,25 @@
 from aiogram.types import BotCommand
 
 admin_commands = main_menu_commands = [
+    BotCommand(command='order_add', description='Создать заказ'),
+    BotCommand(command='list_orders', description='Список заказов'),
+    BotCommand(command='customer_add', description='Создать клиента'),
+    BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
     BotCommand(command='cancel', description='Выход'),
-    BotCommand(command='reg', description='Регистрация'),
     BotCommand(command='list_user', description='Инженеры'),
     BotCommand(command='change_perms', description='Изменить пользователя!'),
-    BotCommand(command='deluser', description='Удалить пользователя'),
-    BotCommand(command='listuser', description='Инженеры'),
-    BotCommand(command='customer_add', description='Создать клиента'),
     BotCommand(command='customer_edit', description='Изменить клиента'),
     BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
-    BotCommand(command='order_add', description='Создать заказ'),
-    BotCommand(command='list_orders', description='Список заказов')
 
 ]
 
 user_commands = [
-    BotCommand(command='cancel', description='Выход'),
-    BotCommand(command='reg', description='Регистрация'),
+    BotCommand(command='order_add', description='Создать заказ'),
+    BotCommand(command='list_orders', description='Список заказов'),
     BotCommand(command='customer_add', description='Создать клиента'),
     BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
-    BotCommand(command='order_add', description='Создать заказ'),
-    BotCommand(command='list_orders', description='Список заказов')
-
+    BotCommand(command='customer_add', description='Создать клиента'),
+    BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
+    BotCommand(command='cancel', description='Выход')
 ]
 ungerister_user_commands = [BotCommand(command='reg', description='Регистрация')]
