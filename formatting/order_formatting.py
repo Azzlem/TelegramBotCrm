9cecc55b899e9ed487e0728f7aed20aea511d887
Azzlem <azzlem.sid@gmail.com>
2024-03-13 19:59:29 +0300

    исправил форматирование заказов при выводе их списка добавил условия если нет клиента и инженера в заказе

--- formatting/order_formatting.py
+++ formatting/order_formatting.py
@@ -1,11 +1,25 @@
 async def orders_all(orders):
     answer = ''
     for order in orders:
-        answer += (f"Номер заказа: {order.id}\n"
-                   f"ФИО: {order.customer.fullname}\n"
-                   f"Адрес: {order.customer.address}\n"
-                   f"Телефон: {order.customer.phone}\n"
-                   f"Инженер: {order.user.fullname}\n"
-                   f"Цена заказа: {order.price}\n\n")
+        if order.customer is None:
+            answer += (f"Номер заказа: {order.id}\n"
+                       f"Инженер: {order.user.fullname}\n"
+                       f"Цена заказа: {order.price}\n\n")
+        elif order.user is not None:
+            answer += (f"Номер заказа: {order.id}\n"
+                       f"ФИО: {order.customer.fullname}\n"
+                       f"Адрес: {order.customer.address}\n"
+                       f"Телефон: {order.customer.phone}\n"
+                       f"Цена заказа: {order.price}\n\n")
+        elif order.user is None and order.customer is None:
+            answer += (f"Номер заказа: {order.id}\n"
+                       f"Цена заказа: {order.price}\n\n")
+        else:
+            answer += (f"Номер заказа: {order.id}\n"
+                       f"ФИО: {order.customer.fullname}\n"
+                       f"Адрес: {order.customer.address}\n"
+                       f"Телефон: {order.customer.phone}\n"
+                       f"Инженер: {order.user.fullname}\n"
+                       f"Цена заказа: {order.price}\n\n")
 
     return answer
--- handlers/order_handlers/fsm_list_orders.py
+++ handlers/order_handlers/fsm_list_orders.py
@@ -9,30 +9,36 @@ from actions_base.actions_users import UserActions
 from formatting.order_formatting import orders_all
 from permission import is_owner_admin, is_user, is_registered
 
-logger.add("file_{time}.log", level="ERROR")
-logger.add("file_{time}.log", level='CRITICAL')
+logger.add("file_{time}.json")
 router = Router()
 
 
 @logger.catch
 @router.message(Command(commands='list_orders'))
 async def list_orders(message: Message):
-    user = await UserActions.get_user(message.from_user)
-    if await is_registered(user):
-        await message.answer(text="Заблудился?    Сходи к Админу! Он разблудит! И нехер тыкать проста так кнопочки.")
+    try:
+        user = await UserActions.get_user(message.from_user)
+        if await is_registered(user):
+            await message.answer(
+                text="Заблудился?    Сходи к Админу! Он разблудит! И нехер тыкать проста так кнопочки.")
 
-    elif await is_user(user):
-        orders = await OrdersActions.get_all_orders_with_all_info_for_id(message.from_user)
-        orders = await orders_all(orders)
-        if len(orders) == 0:
-            await message.answer(text="У вас нет активных заказов.")
-        else:
+        elif await is_user(user):
+            orders = await OrdersActions.get_all_orders_with_all_info_for_id(message.from_user)
+            orders = await orders_all(orders)
+            if len(orders) == 0:
+                await message.answer(text="У вас нет активных заказов.")
+
+            else:
+
+                await message.answer(text=orders)
+
+        elif await is_owner_admin(user):
+            orders = await OrdersActions.get_all_orders_with_all_info()
+            orders = await orders_all(orders)
             await message.answer(text=orders)
 
-    elif await is_owner_admin(user):
-        orders = await OrdersActions.get_all_orders_with_all_info()
-        orders = await orders_all(orders)
-        await message.answer(text=orders)
-
-    else:
-        await message.answer(text="Ненадо тыкать незнакомные команды....это плохо влияет на карму.")
+        else:
+            await message.answer(text="Ненадо тыкать незнакомные команды....это плохо влияет на карму.")
+    except:
+        logger.add("file_{time}.json")
+        await message.answer(text="что то не так")
