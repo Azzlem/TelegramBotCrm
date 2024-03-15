f6efc6bacf679a537224c28dd23ae4d7dc2fe037
Azzlem <azzlem.sid@gmail.com>
2024-03-15 21:40:16 +0300

    отредактировал меню команд

--- bot_commands.py
+++ bot_commands.py
@@ -1,7 +1,7 @@
 from aiogram.types import BotCommand
 
 admin_commands = main_menu_commands = [
-    BotCommand(command='order_add', description='Создать заказ'),
+    BotCommand(command='create_order', description='Создать заказ'),
     BotCommand(command='list_orders', description='Список заказов'),
     BotCommand(command='customer_add', description='Создать клиента'),
     BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
@@ -9,17 +9,13 @@ admin_commands = main_menu_commands = [
     BotCommand(command='list_user', description='Инженеры'),
     BotCommand(command='change_perms', description='Изменить пользователя!'),
     BotCommand(command='customer_edit', description='Изменить клиента'),
-    BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
-
 ]
 
 user_commands = [
-    BotCommand(command='order_add', description='Создать заказ'),
+    BotCommand(command='create_order', description='Создать заказ'),
     BotCommand(command='list_orders', description='Список заказов'),
-    BotCommand(command='customer_add', description='Создать клиента'),
     BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
     BotCommand(command='customer_add', description='Создать клиента'),
-    BotCommand(command='item_add', description='Добавить технику (если у вас есть клиент)'),
     BotCommand(command='cancel', description='Выход')
 ]
 ungerister_user_commands = [BotCommand(command='reg', description='Регистрация')]
